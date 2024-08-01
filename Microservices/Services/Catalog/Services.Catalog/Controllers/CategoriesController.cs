68cb62363d1896f303b129919041069a0dce2010
1huseyinyildiz <huseyin_yildiz35@hotmail.com>
2024-08-01 13:05:48 +0200

    Course and Category Controller coding

---
+++ Microservices/Services/Catalog/Services.Catalog/Controllers/CategoriesController.cs
@@ -0,0 +1,42 @@
+﻿using Services.Catalog.Dtos;
+using Services.Catalog.Services;
+using Shared.Shared.ControllerBases;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+namespace Services.Catalog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : CustomBaseController
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var response = await _categoryService.GetAllAsync();
+            return CreateActionResultInstance(response);
+        }
+
+        //api/categories/id
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(string id)
+        {
+            var response = await _categoryService.GetByAsync(id);
+            return CreateActionResultInstance(response);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CategoryDto categoryDto)
+        {
+            var response = await _categoryService.CreateAsync(categoryDto);
+            return CreateActionResultInstance(response);
+        }
+
+    }
+}
